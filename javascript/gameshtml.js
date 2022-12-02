612fc36ef9c02363ca5601b2a3c54c2f013d757e
Arthurfnx <furtadoarthur@hotmail.com>
2022-12-01 23:21:52 -0300

    ultimo commit

--- javascript/gameshtml.js
+++ javascript/gameshtml.js
@@ -1,50 +1,47 @@
-function getGameshtml(){
-    // Create a request variable and assign a new XMLHttpRequest object to it.
-    var request = new XMLHttpRequest()
     
-    // Open a new connection, using the GET request on the URL endpoint
-    request.open('GET', 'https://api.rawg.io/api/games?key=fbc979f002af4ae3a8fe26a0f32ef032', true)
-    request.onload =function() {
-      var data = JSON.parse(this.response)
-        var games = data.results   
-    
-    
-      if (request.status >= 200 && request.status < 400) {
-        //data.forEach(game => {
-                var html = ''
-                var cardDiv = document.getElementById("paginaGames");
-                for (var i = 0; i < 20; i++){
-                    if (i%4 ==0 && i !=0){
-                        html +=`</div>`
-                }
-                        if (i%4 ==0) {
-                        html+= `<div class="row">`
-                }
-                        console.log(games[i].name)
-    
-                       html+= ` <div class="col 12 col-sm-12 col-md-6 col-lg-3">
-                        <div class="card" style="width: 18r em;">
-                            <img src="${games[i].background_image}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                              <h5 class="card-title">${games[i].name}</h5>
-                              <p class="card-text">Rating: ${games[i].rating}</p>
-                              <p class="card-text">Lançamento: ${games[i].released}</p>
-                              <a href="detalhes.html?id=${games[i].id}" class="btn btn-primary">Ver Mais</a>
-                            </div>
-                          </div>
-                    </div>`
-            
-                }   
-                html +=`</div>`
-                cardDiv.innerHTML = html;
-            
-      //  })
-      } else {
-        console.log('error')
+    function listGames(page){
+      const pageSize = 20
+
+        if (!page) page = 1
+
+        let url = `https://api.rawg.io/api/games?key=fbc979f002af4ae3a8fe26a0f32ef032&page=${page}&${pageSize}`
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+          let saida =''
+          var cardDiv = document.getElementById("paginaGames");
+          var gamesPlace = document.getElementById("gamesPlace")
+          for(let i=0; i< data.results.length; i++){
+            if (i%4 ==0 && i !=0){
+              saida +=`</div>`
       }
+              if (i%4 ==0) {
+              saida += `<div class="row">`
+      }
+            let games= data.results[i]
+            saida +=` <div class="col 12 col-sm-12 col-md-6 col-lg-3">
+            <div class="card" style="width: 18r em;">
+                <img src="${games.background_image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${games.name}</h5>
+                  <p class="card-text">Rating: ${games.rating}</p>
+                  <p class="card-text">Lançamento: ${games.released}</p>
+                  <a href="detalhes.html?id=${games.id}" class="btn btn-primary">Ver Mais</a>
+                </div>
+              </div>
+        </div>`
+          }
+          cardDiv.innerHTML = saida
+
+          let navBar =`<div>`
+            navBar +=`<button onclick="listGames(${page + 1})" class="btn btn-dark">Próxima</button>`
+            if (page >1){
+              navBar +=`<button onclick="listGames(${page - 1})" class="btn btn-dark">Anterior</button>`
+            }
+            
+              gamesPlace.innerHTML = navBar
+        })
+        
     }
-    
-    // Send request
-    request.send() 
-    }
-    
+
